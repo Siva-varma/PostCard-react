f6d82355731ca0dc6ee13023122e2031a6ea0b8d
siva <sivavarma1999@gmail.com>
2026-03-20 00:17:04 +0530

    reupload

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
         <nav className='bg-gray-400 backdrop-blur-md text-black px-8 h-[10vh] flex justify-between items-center'>
             <h1 className='text-2xl font-bold'>Posts</h1>
             <button onClick={() => setToggle((prev) => !prev)} className='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'>
-              {toggle ? "Close Form" : (<><i className="ri-image-add-line"></i><span className='ms-2'>Add Post</span></>)}
+              {toggle ? "Close" : "Add Post"}
               </button>
         </nav>
     </div>
